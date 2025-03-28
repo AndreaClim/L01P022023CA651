b66eb4a3a4e4bc537d87549add568622528f6f3d
miche <miche@DESKTOP-EEADCHD>
2025-03-27 18:57:54 -0600

    creacion de tablas en clases

---
+++ L01P022023CA651/Models/facultades.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+namespace L01P022023CA651.Models
+{
+    public class facultades
+    {
+        [Key]
+        public int id { get; set; }
+        public string facultad { get; set; }
+    }
+}
