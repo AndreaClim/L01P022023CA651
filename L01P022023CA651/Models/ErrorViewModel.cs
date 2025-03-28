b090f1de697298e14dce4ee98a69c918f07ebc10
miche <miche@DESKTOP-EEADCHD>
2025-03-27 18:37:41 -0600

    Agregar archivos de proyecto.

---
+++ L01P022023CA651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P022023CA651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
